fedbedd8a3e1d6391d3ccfd49797cc39bb5f41d1
Jingyuan Wu (jw26u22) <jw26u22@soton.ac.uk>
2023-10-22 14:53:09 +0100

    22/10/2023

--- D2/TEAMD_DESIGN.v
+++ D2/TEAMD_DESIGN.v
@@ -1,31 +1,29 @@
 // Verilog netlist created by S-Edit 2021.2.0
-// Written on Sun Oct 22 13:18:09 2023
+// Written on Sun Oct 22 14:52:22 2023
 
 // Library:               TEAMD_Schematics1
 // Cell:                  TEAMD_DESIGN
 // View name:             HALFADDER_SIM
 // Design path:           C:\Users\jw26u22\Downloads\D2-groupd-central\D2\Schematic\TEAMD_Schematics1
 // Control property name(s): VERILOG
-// Exclude global pins on subcircuits: <yes>
+// Exclude global pins on subcircuits: <no>
 
 
-
-module Design_Globals(
-);
-wire Vdd;
-wire Gnd;
-endmodule // Design_Globals
 
 module TEAMD_4_bit_regesister(
 	ClockSeq,
 	DataIn,
+	Gnd,
 	MatchAll,
-	nResetSeq
+	nResetSeq,
+	Vdd
 );
 input ClockSeq;
 input DataIn;
+inout Gnd;
 output MatchAll;
 input nResetSeq;
+inout Vdd;
 
 wire N_1;
 wire N_2;
@@ -65,183 +63,245 @@ wire N_34;
 DFFR DFFR_1(
 	.Clk(ClockSeq),
 	.D(N_22),
+	.Gnd(Gnd),
 	.nQ(N_24),
 	.nRst(nResetSeq),
-	.Q(N_5)
+	.Q(N_5),
+	.Vdd(Vdd)
 );
 DFFR DFFR_2(
 	.Clk(ClockSeq),
 	.D(N_1),
+	.Gnd(Gnd),
 	.nQ(N_25),
 	.nRst(nResetSeq),
-	.Q(N_4)
+	.Q(N_4),
+	.Vdd(Vdd)
 );
 DFFR DFFR_3(
 	.Clk(ClockSeq),
 	.D(N_3),
+	.Gnd(Gnd),
 	.nQ(N_26),
 	.nRst(nResetSeq),
-	.Q(N_30)
+	.Q(N_30),
+	.Vdd(Vdd)
 );
 DFFR DFFR_4(
 	.Clk(ClockSeq),
 	.D(N_18),
+	.Gnd(Gnd),
 	.nQ(N_27),
 	.nRst(nResetSeq),
-	.Q(N_32)
+	.Q(N_32),
+	.Vdd(Vdd)
 );
 INV1 INV1_1(
 	.A(N_11),
-	.Q(N_1)
+	.Gnd(Gnd),
+	.Q(N_1),
+	.Vdd(Vdd)
 );
 INV1 INV1_2(
 	.A(N_2),
-	.Q(N_6)
+	.Gnd(Gnd),
+	.Q(N_6),
+	.Vdd(Vdd)
 );
 INV1 INV1_3(
 	.A(N_17),
-	.Q(N_3)
+	.Gnd(Gnd),
+	.Q(N_3),
+	.Vdd(Vdd)
 );
 INV1 INV1_4(
 	.A(DataIn),
-	.Q(N_7)
+	.Gnd(Gnd),
+	.Q(N_7),
+	.Vdd(Vdd)
 );
 INV1 INV1_5(
 	.A(N_34),
-	.Q(MatchAll)
+	.Gnd(Gnd),
+	.Q(MatchAll),
+	.Vdd(Vdd)
 );
 NAND2 NAND2_1(
 	.A(N_12),
 	.B(N_9),
-	.Q(N_8)
+	.Gnd(Gnd),
+	.Q(N_8),
+	.Vdd(Vdd)
 );
 NAND2 NAND2_2(
 	.A(N_26),
 	.B(N_33),
-	.Q(N_11)
+	.Gnd(Gnd),
+	.Q(N_11),
+	.Vdd(Vdd)
 );
 NAND2 NAND2_3(
 	.A(N_23),
 	.B(N_21),
-	.Q(N_14)
+	.Gnd(Gnd),
+	.Q(N_14),
+	.Vdd(Vdd)
 );
 NAND2 NAND2_4(
 	.A(DataIn),
 	.B(N_14),
-	.Q(N_17)
+	.Gnd(Gnd),
+	.Q(N_17),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_1(
 	.A(N_5),
 	.B(N_26),
 	.C(DataIn),
-	.Q(N_9)
+	.Gnd(Gnd),
+	.Q(N_9),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_2(
 	.A(N_24),
 	.B(N_30),
 	.C(N_7),
-	.Q(N_12)
+	.Gnd(Gnd),
+	.Q(N_12),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_3(
 	.A(N_8),
 	.B(N_25),
 	.C(N_32),
-	.Q(N_15)
+	.Gnd(Gnd),
+	.Q(N_15),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_4(
 	.A(N_13),
 	.B(N_10),
 	.C(N_15),
-	.Q(N_18)
+	.Gnd(Gnd),
+	.Q(N_18),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_5(
 	.A(N_27),
 	.B(N_5),
 	.C(N_26),
-	.Q(N_20)
+	.Gnd(Gnd),
+	.Q(N_20),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_6(
 	.A(N_20),
 	.B(N_19),
 	.C(N_16),
-	.Q(N_22)
+	.Gnd(Gnd),
+	.Q(N_22),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_7(
 	.A(N_24),
 	.B(N_4),
 	.C(DataIn),
-	.Q(N_31)
+	.Gnd(Gnd),
+	.Q(N_31),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_8(
 	.A(N_5),
 	.B(N_4),
 	.C(N_7),
-	.Q(N_29)
+	.Gnd(Gnd),
+	.Q(N_29),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_9(
 	.A(N_32),
 	.B(N_5),
 	.C(N_7),
-	.Q(N_28)
+	.Gnd(Gnd),
+	.Q(N_28),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_10(
 	.A(N_28),
 	.B(N_29),
 	.C(N_31),
-	.Q(N_33)
+	.Gnd(Gnd),
+	.Q(N_33),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_11(
 	.A(DataIn),
 	.B(N_30),
 	.C(N_32),
-	.Q(N_34)
+	.Gnd(Gnd),
+	.Q(N_34),
+	.Vdd(Vdd)
 );
 NAND4 NAND4_1(
 	.A(N_27),
 	.B(N_24),
 	.C(DataIn),
 	.D(N_6),
-	.Q(N_10)
+	.Gnd(Gnd),
+	.Q(N_10),
+	.Vdd(Vdd)
 );
 NAND4 NAND4_2(
 	.A(N_27),
 	.B(N_5),
 	.C(N_26),
 	.D(N_7),
-	.Q(N_13)
+	.Gnd(Gnd),
+	.Q(N_13),
+	.Vdd(Vdd)
 );
 NAND4 NAND4_3(
 	.A(N_32),
 	.B(N_24),
 	.C(N_26),
 	.D(DataIn),
-	.Q(N_16)
+	.Gnd(Gnd),
+	.Q(N_16),
+	.Vdd(Vdd)
 );
 NAND4 NAND4_4(
 	.A(N_32),
 	.B(N_24),
 	.C(N_25),
 	.D(N_30),
-	.Q(N_19)
+	.Gnd(Gnd),
+	.Q(N_19),
+	.Vdd(Vdd)
 );
 NAND4 NAND4_5(
 	.A(N_26),
 	.B(N_4),
 	.C(N_5),
 	.D(N_32),
-	.Q(N_21)
+	.Gnd(Gnd),
+	.Q(N_21),
+	.Vdd(Vdd)
 );
 NAND4 NAND4_6(
 	.A(N_30),
 	.B(N_25),
 	.C(N_24),
 	.D(N_27),
-	.Q(N_23)
+	.Gnd(Gnd),
+	.Q(N_23),
+	.Vdd(Vdd)
 );
 NOR2 NOR2_1(
 	.A(N_26),
 	.B(N_25),
-	.Q(N_2)
+	.Gnd(Gnd),
+	.Q(N_2),
+	.Vdd(Vdd)
 );
 endmodule // TEAMD_4_bit_regesister
 
@@ -250,18 +310,24 @@ module TEAMD_9_bit_recogniser(
 	A3,
 	A4,
 	A5,
-	Q2
+	Gnd,
+	Q2,
+	Vdd
 );
 input A3;
 input A4;
 input A5;
+inout Gnd;
 output Q2;
+inout Vdd;
 
 TEAMD_4_bit_regesister TEAMD_4_bit_regesister_1(
 	.ClockSeq(A3),
 	.DataIn(A5),
+	.Gnd(Gnd),
 	.MatchAll(Q2),
-	.nResetSeq(A4)
+	.nResetSeq(A4),
+	.Vdd(Vdd)
 );
 endmodule // TEAMD_9_bit_recogniser
 
@@ -269,11 +335,15 @@ endmodule // TEAMD_9_bit_recogniser
 module TEAMD_CIRCUIT1(
 	A1,
 	A2,
-	Q1
+	Gnd,
+	Q1,
+	Vdd
 );
 input A1;
 input A2;
+inout Gnd;
 output Q1;
+inout Vdd;
 
 wire N_1;
 wire N_2;
@@ -323,189 +393,267 @@ wire N_44;
 DFFR DFFR_2(
 	.Clk(N_37),
 	.D(N_38),
+	.Gnd(Gnd),
 	.nQ(N_38),
 	.nRst(A1),
-	.Q(N_5)
+	.Q(N_5),
+	.Vdd(Vdd)
 );
 DFFR DFFR_10(
 	.Clk(N_38),
 	.D(N_39),
+	.Gnd(Gnd),
 	.nQ(N_39),
 	.nRst(A1),
-	.Q(N_14)
+	.Q(N_14),
+	.Vdd(Vdd)
 );
 DFFR DFFR_11(
 	.Clk(N_39),
 	.D(N_40),
+	.Gnd(Gnd),
 	.nQ(N_40),
 	.nRst(A1),
-	.Q(N_17)
+	.Q(N_17),
+	.Vdd(Vdd)
 );
 DFFR DFFR_12(
 	.Clk(N_40),
 	.D(N_41),
+	.Gnd(Gnd),
 	.nQ(N_41),
 	.nRst(A1),
-	.Q(N_20)
+	.Q(N_20),
+	.Vdd(Vdd)
 );
 DFFR DFFR_13(
 	.Clk(N_41),
 	.D(N_42),
+	.Gnd(Gnd),
 	.nQ(N_42),
 	.nRst(A1),
-	.Q(N_23)
+	.Q(N_23),
+	.Vdd(Vdd)
 );
 DFFR DFFR_14(
 	.Clk(N_42),
 	.D(N_43),
+	.Gnd(Gnd),
 	.nQ(N_43),
 	.nRst(A1),
-	.Q(N_26)
+	.Q(N_26),
+	.Vdd(Vdd)
 );
 DFFR DFFR_15(
 	.Clk(N_43),
 	.D(N_44),
+	.Gnd(Gnd),
 	.nQ(N_44),
 	.nRst(A1),
-	.Q(N_29)
+	.Q(N_29),
+	.Vdd(Vdd)
 );
 DFFR DFFR_16(
 	.Clk(N_44),
 	.D(N_36),
+	.Gnd(Gnd),
 	.nQ(N_36),
 	.nRst(A1),
-	.Q(Q1)
+	.Q(Q1),
+	.Vdd(Vdd)
 );
 INV1 INV1_1(
 	.A(N_3),
-	.Q(N_2)
+	.Gnd(Gnd),
+	.Q(N_2),
+	.Vdd(Vdd)
 );
 INV1 INV1_2(
 	.A(N_2),
-	.Q(N_4)
+	.Gnd(Gnd),
+	.Q(N_4),
+	.Vdd(Vdd)
 );
 INV1 INV1_3(
 	.A(N_4),
-	.Q(N_6)
+	.Gnd(Gnd),
+	.Q(N_6),
+	.Vdd(Vdd)
 );
 INV1 INV1_4(
 	.A(N_6),
-	.Q(N_7)
+	.Gnd(Gnd),
+	.Q(N_7),
+	.Vdd(Vdd)
 );
 INV1 INV1_5(
 	.A(N_7),
-	.Q(N_8)
+	.Gnd(Gnd),
+	.Q(N_8),
+	.Vdd(Vdd)
 );
 INV1 INV1_6(
 	.A(N_8),
-	.Q(N_9)
+	.Gnd(Gnd),
+	.Q(N_9),
+	.Vdd(Vdd)
 );
 INV1 INV1_7(
 	.A(N_9),
-	.Q(N_10)
+	.Gnd(Gnd),
+	.Q(N_10),
+	.Vdd(Vdd)
 );
 INV1 INV1_8(
 	.A(N_10),
-	.Q(N_11)
+	.Gnd(Gnd),
+	.Q(N_11),
+	.Vdd(Vdd)
 );
 INV1 INV1_9(
 	.A(N_11),
-	.Q(N_12)
+	.Gnd(Gnd),
+	.Q(N_12),
+	.Vdd(Vdd)
 );
 INV1 INV1_10(
 	.A(N_12),
-	.Q(N_13)
+	.Gnd(Gnd),
+	.Q(N_13),
+	.Vdd(Vdd)
 );
 INV1 INV1_11(
 	.A(N_13),
-	.Q(N_16)
+	.Gnd(Gnd),
+	.Q(N_16),
+	.Vdd(Vdd)
 );
 INV1 INV1_12(
 	.A(N_16),
-	.Q(N_19)
+	.Gnd(Gnd),
+	.Q(N_19),
+	.Vdd(Vdd)
 );
 INV1 INV1_13(
 	.A(N_19),
-	.Q(N_22)
+	.Gnd(Gnd),
+	.Q(N_22),
+	.Vdd(Vdd)
 );
 INV1 INV1_14(
 	.A(N_22),
-	.Q(N_25)
+	.Gnd(Gnd),
+	.Q(N_25),
+	.Vdd(Vdd)
 );
 INV1 INV1_15(
 	.A(N_25),
-	.Q(N_28)
+	.Gnd(Gnd),
+	.Q(N_28),
+	.Vdd(Vdd)
 );
 INV1 INV1_16(
 	.A(N_28),
-	.Q(N_31)
+	.Gnd(Gnd),
+	.Q(N_31),
+	.Vdd(Vdd)
 );
 INV1 INV1_17(
 	.A(N_31),
-	.Q(N_33)
+	.Gnd(Gnd),
+	.Q(N_33),
+	.Vdd(Vdd)
 );
 INV1 INV1_18(
 	.A(N_33),
-	.Q(N_34)
+	.Gnd(Gnd),
+	.Q(N_34),
+	.Vdd(Vdd)
 );
 INV1 INV1_19(
 	.A(N_34),
-	.Q(N_35)
+	.Gnd(Gnd),
+	.Q(N_35),
+	.Vdd(Vdd)
 );
 INV1 INV1_20(
 	.A(N_35),
-	.Q(N_15)
+	.Gnd(Gnd),
+	.Q(N_15),
+	.Vdd(Vdd)
 );
 INV1 INV1_21(
 	.A(N_15),
-	.Q(N_18)
+	.Gnd(Gnd),
+	.Q(N_18),
+	.Vdd(Vdd)
 );
 INV1 INV1_22(
 	.A(N_18),
-	.Q(N_21)
+	.Gnd(Gnd),
+	.Q(N_21),
+	.Vdd(Vdd)
 );
 INV1 INV1_23(
 	.A(N_21),
-	.Q(N_24)
+	.Gnd(Gnd),
+	.Q(N_24),
+	.Vdd(Vdd)
 );
 INV1 INV1_24(
 	.A(N_24),
-	.Q(N_27)
+	.Gnd(Gnd),
+	.Q(N_27),
+	.Vdd(Vdd)
 );
 INV1 INV1_25(
 	.A(N_27),
-	.Q(N_30)
+	.Gnd(Gnd),
+	.Q(N_30),
+	.Vdd(Vdd)
 );
 INV1 INV1_26(
 	.A(N_30),
-	.Q(N_32)
+	.Gnd(Gnd),
+	.Q(N_32),
+	.Vdd(Vdd)
 );
 INV1 INV1_27(
 	.A(N_32),
-	.Q(N_37)
+	.Gnd(Gnd),
+	.Q(N_37),
+	.Vdd(Vdd)
 );
 MUX2 MUX2_1(
 	.A(N_1),
 	.B(N_37),
+	.Gnd(Gnd),
 	.Q(N_3),
-	.S(A2)
+	.S(A2),
+	.Vdd(Vdd)
 );
 TIE1 TIE1_1(
-	.Q(N_1)
+	.Gnd(Gnd),
+	.Q(N_1),
+	.Vdd(Vdd)
 );
 endmodule // TEAMD_CIRCUIT1
 
 
 module TEAMD_Control_Unit(
 	CLK,
+	Gnd,
 	iLoad,
 	nReset,
-	Rx
+	Rx,
+	Vdd
 );
 input CLK;
+inout Gnd;
 output iLoad;
 input nReset;
 input Rx;
+inout Vdd;
 
 wire N_1;
 wire N_2;
@@ -535,140 +683,183 @@ wire N_24;
 DFFR DFFR_1(
 	.Clk(CLK),
 	.D(N_6),
+	.Gnd(Gnd),
 	.nQ(N_21),
 	.nRst(nReset),
-	.Q(N_1)
+	.Q(N_1),
+	.Vdd(Vdd)
 );
 DFFR DFFR_2(
 	.Clk(CLK),
 	.D(N_12),
+	.Gnd(Gnd),
 	.nQ(N_22),
 	.nRst(nReset),
-	.Q(N_3)
+	.Q(N_3),
+	.Vdd(Vdd)
 );
 DFFR DFFR_3(
 	.Clk(CLK),
 	.D(N_9),
+	.Gnd(Gnd),
 	.nQ(N_23),
 	.nRst(nReset),
-	.Q(N_4)
+	.Q(N_4),
+	.Vdd(Vdd)
 );
 DFFR DFFR_4(
 	.Clk(CLK),
 	.D(N_17),
+	.Gnd(Gnd),
 	.nQ(N_24),
 	.nRst(nReset),
-	.Q(N_5)
+	.Q(N_5),
+	.Vdd(Vdd)
 );
 INV1 INV1_1(
 	.A(Rx),
-	.Q(N_2)
+	.Gnd(Gnd),
+	.Q(N_2),
+	.Vdd(Vdd)
 );
 NAND2 NAND2_1(
 	.A(N_8),
 	.B(N_11),
-	.Q(N_6)
+	.Gnd(Gnd),
+	.Q(N_6),
+	.Vdd(Vdd)
 );
 NAND2 NAND2_2(
 	.A(N_14),
 	.B(N_16),
-	.Q(N_9)
+	.Gnd(Gnd),
+	.Q(N_9),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_1(
 	.A(N_21),
 	.B(N_3),
 	.C(N_23),
-	.Q(N_7)
+	.Gnd(Gnd),
+	.Q(N_7),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_2(
 	.A(N_21),
 	.B(N_3),
 	.C(N_24),
-	.Q(N_10)
+	.Gnd(Gnd),
+	.Q(N_10),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_3(
 	.A(N_13),
 	.B(N_7),
 	.C(N_10),
-	.Q(N_12)
+	.Gnd(Gnd),
+	.Q(N_12),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_4(
 	.A(N_21),
 	.B(N_23),
 	.C(N_5),
-	.Q(N_14)
+	.Gnd(Gnd),
+	.Q(N_14),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_5(
 	.A(N_21),
 	.B(N_4),
 	.C(N_24),
-	.Q(N_16)
+	.Gnd(Gnd),
+	.Q(N_16),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_6(
 	.A(N_21),
 	.B(N_4),
 	.C(N_24),
-	.Q(N_18)
+	.Gnd(Gnd),
+	.Q(N_18),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_7(
 	.A(N_21),
 	.B(N_3),
 	.C(N_24),
-	.Q(N_19)
+	.Gnd(Gnd),
+	.Q(N_19),
+	.Vdd(Vdd)
 );
 NAND3 NAND3_8(
 	.A(N_2),
 	.B(N_21),
 	.C(N_24),
-	.Q(N_20)
+	.Gnd(Gnd),
+	.Q(N_20),
+	.Vdd(Vdd)
 );
 NAND4 NAND4_1(
 	.A(N_1),
 	.B(N_22),
 	.C(N_23),
 	.D(N_24),
-	.Q(N_8)
+	.Gnd(Gnd),
+	.Q(N_8),
+	.Vdd(Vdd)
 );
 NAND4 NAND4_2(
 	.A(N_21),
 	.B(N_3),
 	.C(N_4),
 	.D(N_5),
-	.Q(N_11)
+	.Gnd(Gnd),
+	.Q(N_11),
+	.Vdd(Vdd)
 );
 NAND4 NAND4_3(
 	.A(N_21),
 	.B(N_22),
 	.C(N_4),
 	.D(N_5),
-	.Q(N_13)
+	.Gnd(Gnd),
+	.Q(N_13),
+	.Vdd(Vdd)
 );
 NAND4 NAND4_4(
 	.A(N_1),
 	.B(N_22),
 	.C(N_23),
 	.D(N_24),
-	.Q(N_15)
+	.Gnd(Gnd),
+	.Q(N_15),
+	.Vdd(Vdd)
 );
 NAND4 NAND4_5(
 	.A(N_18),
 	.B(N_15),
 	.C(N_19),
 	.D(N_20),
-	.Q(N_17)
+	.Gnd(Gnd),
+	.Q(N_17),
+	.Vdd(Vdd)
 );
 NOR4 NOR4_1(
 	.A(N_21),
 	.B(N_3),
 	.C(N_4),
 	.D(N_24),
-	.Q(iLoad)
+	.Gnd(Gnd),
+	.Q(iLoad),
+	.Vdd(Vdd)
 );
 endmodule // TEAMD_Control_Unit
 
 
 module TEAMD_Asynchronous_Serial_Interface(
 	CLK,
+	Gnd,
 	iD0,
 	iD1,
 	iD2,
@@ -678,9 +869,11 @@ module TEAMD_Asynchronous_Serial_Interface(
 	iD6,
 	iLoad,
 	nReset,
-	Rx
+	Rx,
+	Vdd
 );
 input CLK;
+inout Gnd;
 output iD0;
 output iD1;
 output iD2;
@@ -691,6 +884,7 @@ output iD6;
 output iLoad;
 input nReset;
 input Rx;
+inout Vdd;
 
 wire N_1;
 wire N_2;
@@ -703,57 +897,73 @@ wire N_7;
 DFFR DFFR_1(
 	.Clk(CLK),
 	.D(Rx),
+	.Gnd(Gnd),
 	.nQ(N_1),
 	.nRst(nReset),
-	.Q(iD6)
+	.Q(iD6),
+	.Vdd(Vdd)
 );
 DFFR DFFR_2(
 	.Clk(CLK),
 	.D(iD6),
+	.Gnd(Gnd),
 	.nQ(N_2),
 	.nRst(nReset),
-	.Q(iD5)
+	.Q(iD5),
+	.Vdd(Vdd)
 );
 DFFR DFFR_3(
 	.Clk(CLK),
 	.D(iD5),
+	.Gnd(Gnd),
 	.nQ(N_3),
 	.nRst(nReset),
-	.Q(iD4)
+	.Q(iD4),
+	.Vdd(Vdd)
 );
 DFFR DFFR_4(
 	.Clk(CLK),
 	.D(iD4),
+	.Gnd(Gnd),
 	.nQ(N_4),
 	.nRst(nReset),
-	.Q(iD3)
+	.Q(iD3),
+	.Vdd(Vdd)
 );
 DFFR DFFR_5(
 	.Clk(CLK),
 	.D(iD3),
+	.Gnd(Gnd),
 	.nQ(N_5),
 	.nRst(nReset),
-	.Q(iD2)
+	.Q(iD2),
+	.Vdd(Vdd)
 );
 DFFR DFFR_6(
 	.Clk(CLK),
 	.D(iD2),
+	.Gnd(Gnd),
 	.nQ(N_6),
 	.nRst(nReset),
-	.Q(iD1)
+	.Q(iD1),
+	.Vdd(Vdd)
 );
 DFFR DFFR_7(
 	.Clk(CLK),
 	.D(iD1),
+	.Gnd(Gnd),
 	.nQ(N_7),
 	.nRst(nReset),
-	.Q(iD0)
+	.Q(iD0),
+	.Vdd(Vdd)
 );
 TEAMD_Control_Unit TEAMD_Control_Unit_2(
 	.CLK(CLK),
+	.Gnd(Gnd),
 	.iLoad(iLoad),
 	.nReset(nReset),
-	.Rx(Rx)
+	.Rx(Rx),
+	.Vdd(Vdd)
 );
 endmodule // TEAMD_Asynchronous_Serial_Interface
 
@@ -768,6 +978,7 @@ module TEAMD_DESIGN(
 	A6,
 	A8,
 	A9,
+	Gnd,
 	Q0,
 	Q1,
 	Q2,
@@ -778,7 +989,8 @@ module TEAMD_DESIGN(
 	Q7,
 	Q8,
 	Q9,
-	Q11
+	Q11,
+	Vdd
 );
 input A0;
 input A1;
@@ -789,6 +1001,7 @@ input A5;
 input A6;
 input A8;
 input A9;
+inout Gnd;
 output Q0;
 output Q1;
 output Q2;
@@ -800,19 +1013,25 @@ output Q7;
 output Q8;
 output Q9;
 output Q11;
+inout Vdd;
 
 INV1 INV1_1(
 	.A(A0),
-	.Q(Q0)
+	.Gnd(Gnd),
+	.Q(Q0),
+	.Vdd(Vdd)
 );
 TEAMD_9_bit_recogniser TEAMD_9_bit_recogniser_1(
 	.A3(A3),
 	.A4(A4),
 	.A5(A5),
-	.Q2(Q2)
+	.Gnd(Gnd),
+	.Q2(Q2),
+	.Vdd(Vdd)
 );
 TEAMD_Asynchronous_Serial_Interface TEAMD_Asynchronous_Serial_Interface_1(
 	.CLK(A6),
+	.Gnd(Gnd),
 	.iD0(Q3),
 	.iD1(Q4),
 	.iD2(Q5),
@@ -822,12 +1041,15 @@ TEAMD_Asynchronous_Serial_Interface TEAMD_Asynchronous_Serial_Interface_1(
 	.iD6(Q9),
 	.iLoad(Q11),
 	.nReset(A8),
-	.Rx(A9)
+	.Rx(A9),
+	.Vdd(Vdd)
 );
 TEAMD_CIRCUIT1 TEAMD_RINGOSC_1(
 	.A1(A1),
 	.A2(A2),
-	.Q1(Q1)
+	.Gnd(Gnd),
+	.Q1(Q1),
+	.Vdd(Vdd)
 );
 endmodule // TEAMD_DESIGN
 
